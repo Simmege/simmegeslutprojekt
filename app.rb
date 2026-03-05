5fe316a9731ef1e505f579418a5b89edefaf5cfb
Simmege <simon.erlen@outlook.com>
2026-03-05 16:44:07 +0100

    2026-03-05

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
+require 'sinatra/base'
+require 'sqlite3'
 require 'debug'
-require "awesome_print"
+require 'awesome_print'
 
+DB_PATH = 'db/strong_bakes.db'
 class App < Sinatra::Base
 
     setup_development_features(self)
@@ -14,15 +17,43 @@ class App < Sinatra::Base
       return @db
     end
 
-    # Routen /
-    get '/' do
-        erb (:"views/index")
+
+
+
+  get '/products' do
+    @products = db.execute('SELECT * FROM products')
+   erb :index
+  end
+    
+
+    get '/add/:id' do
+      db = SQLite3::Database.new('db/strong_bakes.db')
+      db.execute("INSERT INTO cart (product_id, quantity) VALUES (?, 1)", params[:id])
+      redirect '/products'
     end
 
-    get '/products' do
-      @products = db.execute('SELECT * FROM products')
-      erb (:"views/index")
+    get '/cart' do
+      db = SQLite3::Database.new('db/strong_bakes.db')
+      db.results_as_hash = true
+      
+      @items = db.execute("
+        SELECT cart.id AS cart_id, products.smak, products.pris, cart.quantity 
+        FROM cart 
+        JOIN products ON cart.product_id = products.id")
+      
+      @total = 0
+      @items.each do |item|
+      @total += item['pris'] * item['quantity']
+      end
+      erb :cart
     end
+
+    post '/cart/:id/delete' do |id|
+      db.execute("DELETE FROM cart WHERE id =?",[id]).first
+  
+      redirect ("/cart")
+    end
+
     
 
 end
--- db/seeder.rb
+++ db/seeder.rb
@@ -1,14 +1,17 @@
 require 'sqlite3'
 
 class Seeder
-
   DB_PATH = 'db/strong_bakes.db'
 
   def self.seed!
-    puts "Klart! Strong Bakes databas är redo."
+    drop_tables
+    create_tables
+    populate_tables
+    puts "Klart! Strong Bakes databas är redo med varukorg."
   end
 
   def self.drop_tables
+    db.execute('DROP TABLE IF EXISTS cart')
     db.execute('DROP TABLE IF EXISTS products')
   end
 
@@ -16,16 +19,23 @@ class Seeder
     db.execute('CREATE TABLE products (
                   id INTEGER PRIMARY KEY AUTOINCREMENT,
                   smak TEXT NOT NULL,
-                  topping TEXT NOT NULL,
-                  pris TEXT,
+                  pris INTEGER NOT NULL,
                   category_id INTEGER)')
+
+    db.execute('CREATE TABLE cart (
+                  id INTEGER PRIMARY KEY AUTOINCREMENT,
+                  product_id INTEGER NOT NULL,
+                  quantity INTEGER)')
   end
 
   def self.populate_tables
-    db.execute('INSERT INTO products (smak,  pris) VALUES ("Choco-Muscle Crunch", "25kr")')
-    db.execute('INSERT INTO products (smak,  pris) VALUES ("Salted Caramel Pump", "25kr")')
-    db.execute('INSERT INTO products (smak,  pris) VALUES ("Vanilla Power Swirl", "25kr")')
-    db.execute('INSERT INTO products (smak,  pris) VALUES ("Apple Beast", "25kr")')
+    db.execute('INSERT INTO products (smak, pris) VALUES ("Choco-Muscle Crunch", 25)')
+    db.execute('INSERT INTO products (smak, pris) VALUES ("Salted Caramel Pump", 25)')
+    db.execute('INSERT INTO products (smak, pris) VALUES ("Vanilla Power Swirl", 30)')
+    db.execute('INSERT INTO products (smak, pris) VALUES ("Apple Beast", 20)')
+    
+
+    db.execute('INSERT INTO cart (product_id, quantity) VALUES (1, 2)')
   end
 
   private
@@ -33,11 +43,14 @@ class Seeder
   def self.db
     @db ||= begin
       Dir.mkdir('db') unless Dir.exist?('db')
-      
       db = SQLite3::Database.new(DB_PATH)
       db.results_as_hash = true
       db
     end
   end
 end
+
+Seeder.drop_tables
+Seeder.create_tables
+Seeder.populate_tables
 Seeder.seed!
